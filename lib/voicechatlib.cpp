2a7063985ded034ef8f6a9ed4e041914bef63034
entity12208 <116198380+entity12208@users.noreply.github.com>
2024-07-31 08:55:55 -0700

    Update voicechatlib.cpp

--- lib/voicechatlib.cpp
+++ lib/voicechatlib.cpp
@@ -1,43 +1,34 @@
-#include <iostream>
-#include <discord.h>
-
-class VoiceChatLib {
-private:
-    static bool isDeafened;
-    static DiscordClient* discordClient;
-    static std::string voiceChannelId;
-
-public:
-    static void initialize() {
-        connectToVoiceChatServer();
-    }
-
-    static void connectToVoiceChatServer() {
-        discordClient = new DiscordClient(MTI2Nzg4MDA0NDc0Mzg4NDg2MQ.GBCtfg.2OaCatoXPoWbEITPw5lZvJ-gK2P_sinzVxQs7Y
-);
-        voiceChannelId = 1267881280624922647;
-        discordClient->connect();
-        std::cout << "Connected to voice chat server" << std::endl;
-    }
-
-    static void deafen() {
-        if (!isDeafened) {
-            discordClient->muteSelf();
-            std::cout << "User deafened" << std::endl;
-            isDeafened = true;
-        }
-    }
-
-    static void undeafen() {
-        if (isDeafened) {
-            discordClient->unmuteSelf();
-            std::cout << "User undeafened" << std::endl;
-            isDeafened = false;
-        }
-    }
-};
+#include "voicechatlib.h"
 
 // Initialize static members
 bool VoiceChatLib::isDeafened = false;
 DiscordClient* VoiceChatLib::discordClient = nullptr;
 std::string VoiceChatLib::voiceChannelId = "";
+
+void VoiceChatLib::initialize() {
+    connectToVoiceChatServer();
+}
+
+void VoiceChatLib::connectToVoiceChatServer() {
+    discordClient = new DiscordClient("YOUR_DISCORD_BOT_TOKEN");
+    voiceChannelId = "YOUR_VOICE_CHANNEL_ID";
+    discordClient->connect();
+    discordClient->joinVoiceChannel(voiceChannelId);
+    std::cout << "Connected to voice chat server" << std::endl;
+}
+
+void VoiceChatLib::deafen() {
+    if (!isDeafened) {
+        discordClient->muteSelf();
+        std::cout << "User deafened" << std::endl;
+        isDeafened = true;
+    }
+}
+
+void VoiceChatLib::undeafen() {
+    if (isDeafened) {
+        discordClient->unmuteSelf();
+        std::cout << "User undeafened" << std::endl;
+        isDeafened = false;
+    }
+}
